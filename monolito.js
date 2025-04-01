a25f616d1311b11034127d2045722b08fd447eb0
Dominic Nascimento Laubhouet - 822141082 <doominicnascl@gmail.com>
2025-04-01 11:48:31 -0300

    Finalizando servidor monolitico

--- monolito.js
+++ monolito.js
@@ -16,4 +16,16 @@ app.post("/usuarios", (req, res) => { // Rota para cadastrar usuários
 app.get("/dados", (req,res) => {
     res.send({usuarios})
 })
-app.listen(3000, () => console.log("Servidor monolitico ok, na porta 3000")) // Inicializando o servidor na porta 3000
+app.listen(3000, () => console.log("Servidor monolitico ok, na porta 3000")) // Inicializando o servidor na porta 3000 
+
+/*app.post("/pedidos", (req, res) => {
+    const pedidos = req.body
+    pedidos.push(pedidos)
+    res.send({message: "Pedido cadastrado com sucesso", pedidos})
+})
+
+app.get("/dados", (req,res)=> {
+    res.send({pedidos})
+})
+
+app.listen(4000,() => console.log("Servidor monolitico ok, na porta 4000")) // Inicializando o servidor na porta 4000 */
